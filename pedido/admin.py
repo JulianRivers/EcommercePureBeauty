04aea947c2919c3ca9f4396b69e9a1d57bf0f2cd
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-11-03 11:47:54 -0500

    register admin pedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (DetallePedido, EstadoPedido, MetodoPago, Pedido)
+
+admin.site.register(DetallePedido)
+admin.site.register(EstadoPedido)
+admin.site.register(MetodoPago)
+admin.site.register(Pedido)
+
