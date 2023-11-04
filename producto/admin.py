b252bbca2471405718e310d5ddb65b9c9b719f8c
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-11-04 12:53:19 -0500

    registro de models en el panel de administrador

--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Categoria, Producto, ProductoEnCarrito, ProductoSubcategoria, Subcategoria)
+
+admin.site.register(Categoria)
+admin.site.register(ProductoEnCarrito)
+admin.site.register(ProductoSubcategoria)
+admin.site.register(Producto)
+admin.site.register(Subcategoria)
--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Carrito,Ciudad,Departamento,Pais,UserProfile)
+
+admin.site.register(Carrito)
+admin.site.register(Ciudad)
+admin.site.register(Departamento)
+admin.site.register(Pais)
+admin.site.register(UserProfile)
