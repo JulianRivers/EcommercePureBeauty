a62762da10ef3a332ba8c1f2905cf58f5be4f051
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-11-10 07:30:07 -0500

    admin register pedido y producto

--- pedido/admin.py
+++ pedido/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 from .models import (DetallePedido, EstadoPedido, MetodoPago, Pedido)
 
-admin.site.register(DetallePedido)
-admin.site.register(EstadoPedido)
-admin.site.register(MetodoPago)
-admin.site.register(Pedido)
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    pass
 
+@admin.register(MetodoPago)
+class MetodoPagoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EstadoPedido)
+class EstadoPedidoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(DetallePedido)
+class DetallePedidoAdmin(admin.ModelAdmin):
+    pass
--- producto/admin.py
+++ producto/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 
 from .models import (Categoria, Producto, ProductoEnCarrito, ProductoSubcategoria, Subcategoria)
 
-admin.site.register(Categoria)
-admin.site.register(ProductoEnCarrito)
-admin.site.register(ProductoSubcategoria)
-admin.site.register(Producto)
-admin.site.register(Subcategoria)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    # list_display=('id', 'nombre')
+    # list_filter=('nombre',) 
+    ordering=('-nombre',)
+    search_fields=('nombre',)
+
+@admin.register(Subcategoria)
+class SubcategoriaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductoEnCarrito)
+class ProductoCarritoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductoSubcategoria)
+class ProductoSubcategoriaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    pass
+
+
