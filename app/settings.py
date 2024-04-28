bc2d9897da1856b334ebc39074363c443785ccd6
JulianRivers <juliancamilorifo@ufps.edu.co>
2024-04-27 19:01:39 -0500

    icon y statics

--- app/settings.py
+++ app/settings.py
@@ -129,13 +129,16 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static/'),
-   os.path.join(BASE_DIR, 'static/css/'),
-   os.path.join(BASE_DIR, 'static/js/'),
-)
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'static/css'),
+        os.path.join(BASE_DIR, 'static/js'),
+    ]
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #CRISPY
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
